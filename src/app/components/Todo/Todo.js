f8bf388537bf7cbf4e5e9ef09c97e3f84165e624
Andy Carter <acarter@envoc.com>
2018-06-15 17:07:43 -0500

    first commit

---
+++ src/app/components/Todo/Todo.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <div>Todo!</div>
+        );
+    }
+}
